dde01575cfa476be7685e1ef67a8b9211063aea0
Randolf Uy <920265148@sfsu.edu>
2023-10-25 12:00:18 -0700

    Created a drop down menu with built in search functionality

--- public/artists_comparison.js
+++ public/artists_comparison.js
@@ -1,3 +1,44 @@
+function myFunction1() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+}
+  
+function filterFunction1() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput1");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown1");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";    
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
+
+function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+  
+function filterFunction2() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput2");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown2");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";    
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
+
+
 var artist_data1 = [
     {x: "frequency", value: 65},
     {x: "tempo", value: 60},
